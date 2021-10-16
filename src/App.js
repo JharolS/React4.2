31b8406ccfb577bb871689db4aa6626e370289b8
unknown <sv73107535@idat.edu.pe>
2021-10-16 10:49:18 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+import { Route, Switch } from 'react-router-dom';
+import { AddEmployee } from './components/add-employee';
+import { GlobalProvider } from './context/GlobalState';
+import { Home } from './components/home';
+import { EditEmployee } from './components/edit-employee';
+
+function App() {
+  return (
+    <GlobalProvider>
+      <div className="App">
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/add" component={AddEmployee} />
+          <Route path="/edit/:id" component={EditEmployee} />
+        </Switch>
+      </div>
+    </GlobalProvider>
+  );
+}
+    
+export default App;
